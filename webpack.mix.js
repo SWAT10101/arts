f800a32aa6a2a1a6a25661f8ea7581dad6e1ca8a
art <swat1994911@gmail.com>
2019-07-09 00:02:28 +0300

    fix sass mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/materialize-css/dist/js/materialize.js'
+    ]
+   ,'public/js');
+
+mix.sass('resources/sass/app.scss', 'public/css');
