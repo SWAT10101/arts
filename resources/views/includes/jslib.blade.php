41c731fd33778d2fad0fc35de64b38b59ed1a640
art <swat1994911@gmail.com>
2019-07-17 00:52:48 +0300

    add https

--- resources/views/includes/jslib.blade.php
+++ resources/views/includes/jslib.blade.php
@@ -1,8 +1,8 @@
 <!-- js http -->
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<!--<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>-->
 
 <!-- js https -->
-<!--<script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>-->
+<script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
 
 <script>
     $(document).ready(function () {
