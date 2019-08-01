c2648d0296b2e05cd57509f9b739866e2a618716
SWAT <swat1994911@gmail.com>
2019-08-01 14:11:48 +0300

    try fix swal dialog

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.combine([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/materialize-css/dist/js/materialize.js',
     'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
-    'node_modules/sweetalert2/dist/sweetalert2.all.js',
+    'node_modules/sweetalert2/dist/sweetalert2.js',
     ]
    ,'public/js/app.js');
 
