7887d06953ebd6bfecfc4c741072c43611a0aa27
SWAT <swat1994911@gmail.com>
2019-08-01 14:55:11 +0300

    try fix swal dialog

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 
 window.Vue = require('vue');
+window.Swal = require('sweetalert2') 
+
 
 /**
  * The following block of code may be used to automatically register your
--- resources/views/includes/csslib.blade.php
+++ resources/views/includes/csslib.blade.php
@@ -3,10 +3,10 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <!-- Style http -->
-<!--<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">-->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
 <!-- Style https -->
-<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" type="text/css">
+<!--<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" type="text/css">-->
 
 <style>
  
